5d0d9a8d13f50064ad4fbe16a5c3affadfae27bf
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-09-22 19:21:37 +0530

    footer modification

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <span>Ashly Joshy &copy; 2024</span>
-    </div>
+    <footer className="text-center p-5 dark:bg-black dark:text-white">
+      <span className="font-semibold">Ashlyn Joshy &copy; 2024</span>
+    </footer>
   );
 };
 
